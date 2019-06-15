7ad0ce07174c93884eb03903b647a0b2ee9d73aa
Borislav Atanasov <b.v.atanasov@gmail.com>
2019-06-15 18:21:49 +0100

    deploye

---
+++ build/precache-manifest.82ef53f61c702acd27e2b9e52cc235e0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a0d698ed988a6ecd4f0ccb6845e28a",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "19a4d2074c3a55acb278",
+    "url": "/hangman/static/css/main.3d70292d.chunk.css"
+  },
+  {
+    "revision": "7a3bda53408c278ca94f",
+    "url": "/hangman/static/js/2.936e7cab.chunk.js"
+  },
+  {
+    "revision": "19a4d2074c3a55acb278",
+    "url": "/hangman/static/js/main.37e58300.chunk.js"
+  },
+  {
+    "revision": "19ffbe7598942725a8e1",
+    "url": "/hangman/static/js/runtime~main.d9fce119.js"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/hangman/static/media/0.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/hangman/static/media/1.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/hangman/static/media/2.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/hangman/static/media/3.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/hangman/static/media/4.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/hangman/static/media/5.1b557aa7.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/hangman/static/media/6.c80394c2.jpg"
+  }
+]);
